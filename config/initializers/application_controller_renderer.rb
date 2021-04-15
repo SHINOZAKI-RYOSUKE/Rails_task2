fada868121f4ad0e291ef79f315063821e69b334
SHINOZAKI-RYOSUKE <gibson335m@gmail.com>
2021-04-15 07:22:28 +0000

    [update2]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '99fc4c618c529a1c91b52d5e1281164cdc6b8ca88a8dc5e0e6768662d8b1a43e9a9a20d59ef3de99b38ea18367561485ecbd4e8062a31144449b0f1b3c2af928'
