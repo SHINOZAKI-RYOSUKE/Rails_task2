de46fce3502d107f11b0caa7d837d1baed0e1979
SHINOZAKI-RYOSUKE <gibson335m@gmail.com>
2021-04-17 04:45:07 +0000

    [update7]

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,6 +1,6 @@
 class PostImagesController < ApplicationController
-  
-  
+
+
   def new
     @post_image = PostImage.new
   end
@@ -8,7 +8,7 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    
+
     if @post_image.save
       redirect_to post_images_path
     else
@@ -30,8 +30,8 @@ class PostImagesController < ApplicationController
     @post_image.destroy
     redirect_to post_images_path
   end
-  
-  
+
+
 end
 
 
